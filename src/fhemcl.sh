808cb9a2b508c6567576919661a1abd94d3f26ec
sidey79 <7968127+sidey79@users.noreply.github.com>
2020-01-05 21:51:36 +0100

    src/fhemcl.sh
    Optimizes speed by moving url-encode out of bash into curl command which has very much faster implementation

--- src/fhemcl.sh
+++ src/fhemcl.sh
@@ -2,6 +2,8 @@
 # Heinz-Otto Klas 2019
 # send commands to FHEM over HTTP
 # if no Argument, show usage
+# Optimted by S.Butzek 2019-2020
+#set -x
 if [ $# -eq 0 ]
 then
      echo 'fhemcl Usage'
@@ -31,7 +33,7 @@ else
 fi
 
 # get Token via http request
-token=$(curl -s -D - "$hosturl/fhem?XHR=1" | awk '/X-FHEM-csrfToken/{print $2}')
+token=$(curl -s -D - "localhost:8083/fhem?XHR=1" | awk '/^X-FHEM-csrfToken/{print $2}' | tr -d '\r' )
 
 # reading FHEM command, from Pipe, File or Arguments 
 # Check to see if a pipe exists on stdin.
@@ -66,23 +68,24 @@ for ((i=0; i<${#cmdarray[*]}; i++));do
     # concat def lines with ending \ to the next line, remove any \r from line
     cmd=${cmdarray[i]//[$'\r']} 
 
-    while [ "${cmd:${#cmd}-1:1}" = '\' ];do 
-          ((i++))
-          cmd=${cmd::-1}$'\n'${cmdarray[i]//[$'\r']}
-    done
+    #while [ "${cmd:${#cmd}-1:1}" = '\' ];do 
+    #      ((i++))
+    #      cmd=${cmd::-1}$'\n'${cmdarray[i]//[$'\r']}
+    #done
 
     #echo "proceeding Line $((i+1)) : ${cmd}"
     # urlencode loop over String
     cmdu=''
-    for ((pos=0;pos<${#cmd};pos++)); do
-        c=${cmd:$pos:1}
-        [[ "$c" =~ [a-zA-Z0-9\.\~\_\-] ]] || printf -v c '%%%02X' "'$c"
-        cmdu+="$c"
-    done
-    cmd=$cmdu
+    #for ((pos=0;pos<${#cmd};pos++)); do
+    #    c=${cmd:$pos:1}
+    #    [[ "$c" =~ [a-zA-Z0-9\.\~\_\-] ]] || printf -v c '%%%02X' "'$c"
+    #    cmdu+="$c"
+    #done
+    #cmd=$cmdu
     # send command to FHEM and filter the output (tested with list...).
     # give only lines between, including the two Tags back, then remove all HTML Tags 
     #curl -s --data "fwcsrf=$token" "$hosturl/fhem?cmd=$cmd" | sed -n '/<pre>/,/<\/pre>/p' |sed 's/<[^>]*>//g'
     #curl -s --data "fwcsrf=$token" "$hosturl/fhem?cmd=$cmd&XHR=1" | sed -n "/<div id='content' >/,/<\/div>/p" |sed 's/<[^>]*>//g'
-    curl -s --data "fwcsrf=$token" "$hosturl/fhem?cmd=$cmd&XHR=1"
+    #curl -s --data "fwcsrf=$token" "$hosturl/fhem?cmd=$cmd&XHR=1"
+    curl -s "$hosturl/fhem" --data "fwcsrf=$token" --data-urlencode "cmd=$cmd" --data-urlencode "XHR=1"
 done
